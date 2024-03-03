6117cec754c6d6067ac71feac5bf74b15e5b30c3
qloridant <quentin.loridant@protonmail.com>
2024-03-03 18:44:35 +0100

    Use API to fetch barcodes instead of db

--- src/data/elefan_connector.py
+++ src/data/elefan_connector.py
@@ -1,8 +1,6 @@
 from epicerie_connector import EpicerieConnector
-import sys
 import os
 import dotenv
-import sqlalchemy
 
 project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
 dotenv_path = os.path.join(project_dir, ".env")
@@ -12,31 +10,33 @@ dotenv.load_dotenv(dotenv_path)
 class ElefanConnector(EpicerieConnector):
     def __init__(self) -> None:
         super().__init__()
+        self.codes_to_exclude = [
+            21,  # bebe (inclus nourriture)
+            22,  # hygiène, beauté
+            23,  # entretien, nettoyage
+            24,  # cuisine
+            25,  # santé
+            31,  # journaux
+            41,  # maison
+            42,  # papeterie
+            43,  # textile
+            44,  # animalerie
+            45,  # jardinerie
+            90,  # jeux
+        ]
 
-    def _connect(self):
-        # Connect to a DB
-        print(os.environ.get("DB_USERNAME"))
-        try:
-            engine = sqlalchemy.create_engine(
-                f"mysql+pymysql://"
-                f"{os.environ.get('DB_USERNAME')}:{os.environ.get('DB_PASSWORD')}"
-                f"@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}?charset=utf8mb4"
-            )
-        except sqlalchemy.exc as e:
-            print(f"Error connecting to MariaDB Platform: {e}")
-            sys.exit(1)
-
-        return engine
+    def filter_products(self):
+        self.products = [p for p in self.products if p["status"] == "ACTIF"]
+        self.products = [p for p in self.products if p["famille"]["code"] not in self.codes_to_exclude]
 
     def extract_products_codes(self):
-        engine = self._connect()
-        with engine.connect() as con:
-            result = con.execute(sqlalchemy.text("SELECT code FROM kaso.ARTICLE"))
-            codes = [row[0] for row in result]
-        return codes
+        self.extract_products()
+        self.products_codes = [p["code"] for p in self.products]
 
-    def load_products_facts(self):
-        engine = self._connect()
+    def load_products_facts(self, products_facts):
+        self.products_facts = products_facts
+        self.products_facts.to_csv("data/products_facts.csv", sep=";", index=False)
+        engine = self._db_connect()
 
         if len(self.products_facts):
             self.products_facts.to_sql("ARTICLE_FACTS", engine, if_exists="replace")
